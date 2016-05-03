df39d61583d8d20a9a4d82ba4cce2507fe1ff4fe
Casey Allred <callred@liveviewtech.com>
2016-05-03 14:43:43 -0600

    peronsal files

---
+++ personal/movement.el
@@ -0,0 +1,5 @@
+(global-set-key (kbd "M-n") (lambda() (interactive) (next-line 5)))
+(global-set-key (kbd "M-p") (lambda() (interactive) (previous-line 5)))
+
+
+(global-set-key (kbd "M-i") 'mc/mark-next-lines)
