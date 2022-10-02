35fc366fe3b035860eacbfea8a8381e6c05197d0
Casey Allred <sbditto85@gmail.com>
2022-10-01 21:10:22 -0600

    Attempting to force having used packages installed automatically

---
+++ preload/user-packages.el
@@ -0,0 +1,8 @@
+(prelude-require-package 'multiple-cursors)
+(prelude-require-package 'neotree)
+(prelude-require-package 'window-numbering)
+(prelude-require-package 'string-inflection)
+(prelude-require-package 'rg)
+(prelude-require-package 'haskell-mode)
+(prelude-require-package 'erlang)
+(prelude-require-package 'ruby-tools)
