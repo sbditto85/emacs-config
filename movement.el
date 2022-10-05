5a7e41619de8c9b2ab9036a8e1ba1278d0ed3ec2
Casey Allred <sbditto85@gmail.com>
2022-10-04 23:30:02 -0600

    Markdown mode is fixed

--- movement.el
+++ movement.el
@@ -1,2 +1,8 @@
 (global-set-key (kbd "M-n") (lambda() (interactive) (next-line 5)))
 (global-set-key (kbd "M-p") (lambda() (interactive) (previous-line 5)))
+
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-n") nil)
+            (local-set-key (kbd "M-p") nil)))
