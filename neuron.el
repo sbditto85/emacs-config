637ba07138c8b661a178ec7050c63d50fdba5ea8
Casey Allred <callred@wsgr.com>
2022-12-01 09:48:46 -0700

    Project root cd shell pop

--- neuron.el
+++ neuron.el
@@ -2,7 +2,7 @@
   (let ((map (make-sparse-keymap)))
     (define-key map "b" 'magit-blame)
     (define-key map "d" 'insert-date-time)
-    (define-key map "c" 'write-context)
+    (define-key map "c" 'write-contexts)
     (define-key map "f" 'find-file-at-point)
     (define-key map "t" 'shell-pop)
     (define-key map "m" 'write-modules)
@@ -12,3 +12,8 @@
   "Neuron key map.")
 
 (global-set-key (kbd "s-n") neuron-map)
+
+(add-hook 'shell-pop-in-after-hook
+          (lambda ()
+            (term-send-raw-string (concat "cd " (shell-quote-argument (projectile-project-root)) "\n"))
+            (term-send-raw-string "\C-l")))
