e5253b93e1ffa3a2577877e4e38cf60abc9e5821
Casey Allred <callred@wsgr.com>
2022-12-15 11:32:59 -0700

    Ruby compilation package

--- 0_packages.el
+++ 0_packages.el
@@ -11,6 +11,7 @@
 (prelude-require-package 'windresize)
 (prelude-require-package 'ztree)
 (prelude-require-package 'shell-pop)
+(prelude-require-package 'ruby-compilation)
 
 ;; Look into this (Emacs from scratch #2 - Adding UI Improvements 36:00 ish) - Casey, Tue Oct 04 2022
 ;; (prelude-require-package 'helpful)
