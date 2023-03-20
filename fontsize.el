8aaecefdf52f6dff4a71cac50a4127d19b5fa42f
Casey Allred <sbditto85@gmail.com>
2023-03-20 09:29:35 -0600

    Font size can change depending on solo or sharing

--- fontsize.el
+++ fontsize.el
@@ -1,1 +1,11 @@
-(set-face-attribute 'default nil :height 100)
+(defun set-sharing-font-size ()
+  (interactive)
+  (set-face-attribute 'default nil :height 185)
+  )
+
+(defun set-solo-font-size ()
+  (interactive)
+  (set-face-attribute 'default nil :height 150)
+  )
+
+(set-solo-font-size)
