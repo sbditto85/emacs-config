740e1333825ecf2138b286717d14b081ab3f7bf3
Casey Allred <sbditto85@gmail.com>
2024-11-14 09:34:09 -0700

    Font configuration snippet

--- early-init.el
+++ early-init.el
@@ -1,3 +1,15 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+
+;; ;; Used to indicate the desired font
+;; ;; ## Not sure if I want to change the font, leaving here for a record on how to do so in the future
+;; (setq c/monospace-font "JetBrains Mono NL"
+;;       c/monospace-font-size 15)
+
+;; ;; Configure default frame settings before the first frame is shown
+;; (setq default-frame-alist
+;;       (append
+;;        (list
+;;         `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size))))
+;;        default-frame-alist))
