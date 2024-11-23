f7a27af28aa29cb35ff4cd602c7e768364fa9852
Casey Allred <sbditto85@gmail.com>
2024-11-23 10:57:56 -0700

    Default font and size

--- early-init.el
+++ early-init.el
@@ -4,12 +4,12 @@
 
 ;; ;; Used to indicate the desired font
 ;; ;; ## Not sure if I want to change the font, leaving here for a record on how to do so in the future
-;; (setq c/monospace-font "JetBrains Mono NL"
-;;       c/monospace-font-size 15)
+(setq custom/monospace-font "JetBrains Mono NL"
+      custom/monospace-font-size 15)
 
-;; ;; Configure default frame settings before the first frame is shown
-;; (setq default-frame-alist
-;;       (append
-;;        (list
-;;         `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size))))
-;;        default-frame-alist))
+;; Configure default frame settings before the first frame is shown
+(setq default-frame-alist
+      (append
+       (list
+        `(font . ,(concat custom/monospace-font "-" (number-to-string custom/monospace-font-size))))
+       default-frame-alist))
